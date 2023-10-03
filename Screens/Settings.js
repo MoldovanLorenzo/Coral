6d004082e3715a02a44c42b56d2d12395a14eb5d
Simon Darius <139557639+simondarius@users.noreply.github.com>
2023-10-03 11:43:53 +0300

    changes

--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity,ScrolView } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
@@ -36,20 +36,14 @@ export default function Settings() {
 </TouchableOpacity>
 <TouchableOpacity>
 <View style={{paddingLeft:30,height:70,width:300,backgroundColor:'lightgray',borderRadius:30,alignSelf:'center',marginTop:20}}>
-<FontAwesome name="car" size={30} color='gray' style={{position:'relative',top:20}}/>
-<Text style={{alignSelf:'center',fontSize:20,fontWeight:'bold',color:'gray',position:'relative',bottom:10}}>Theme</Text>
-</View>
-</TouchableOpacity>
-<TouchableOpacity>
-<View style={{paddingLeft:30,height:70,width:300,backgroundColor:'lightgray',borderRadius:30,alignSelf:'center',marginTop:20}}>
 <FontAwesome name="language" size={30} color='gray' style={{position:'relative',top:20}}/>
 <Text style={{alignSelf:'center',fontSize:20,fontWeight:'bold',color:'gray',position:'relative',bottom:10}}>Language</Text>
 </View>
 </TouchableOpacity>
 <TouchableOpacity>
 <View style={{paddingLeft:30,height:70,width:300,backgroundColor:'lightgray',borderRadius:30,alignSelf:'center',marginTop:20}}>
-<FontAwesome name="language" size={30} color='gray' style={{position:'relative',top:20}}/>
-<Text style={{alignSelf:'center',fontSize:20,fontWeight:'bold',color:'gray',position:'relative',bottom:10}}>Language</Text>
+<FontAwesome name="star" size={30} color='gray' style={{position:'relative',top:20}}/>
+<Text style={{alignSelf:'center',fontSize:20,fontWeight:'bold',color:'gray',position:'relative',bottom:10}}>Themes</Text>
 </View>
 </TouchableOpacity>
     </View>
