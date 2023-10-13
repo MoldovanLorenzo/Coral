a42e052c660bab6fc8dda0f94935a2c1da036e40
MoldovanLorenzo <139447646+MoldovanLorenzo@users.noreply.github.com>
2023-10-13 11:13:48 +0300

    Update HomeScreen.js

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -64,10 +64,96 @@ const HomeScreen = () => {
   };
 
   return (
-    <View style={{ flex: 1 }}>
-      {/* Restul codului a rămas neschimbat și a fost eliminat marcajul de conflict */}
+      <View style={{ flex: 1 }}>
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity onPress={handleFriendsFinderSelection} style={{ alignSelf: 'center', position: 'relative', left: 20 }}>
+          <FontAwesome name="user-plus" size={25} color="#ff9a00" />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={handleSettingsSelection}
+          style={{ position: 'relative', left: 250, top: 57, margin: 0 }}
+        >
+          <FontAwesome name="gear" size={30} color="#ff9a00" />
+        </TouchableOpacity>
+
+        <Text style={{ alignSelf: 'center', margin: 50, marginLeft: 65, fontSize: 30, fontWeight: 'bold' }}>Chats</Text>
+      </View>
+
+      <View style={{ flexDirection: 'row', width: 250, alignSelf: 'center', borderRadius: 23, backgroundColor: 'lightgray' }}>
+      <View style={{ flexDirection: 'row', width: 250, alignSelf: 'center', borderRadius: 23,backgroundColor:'lightgray' }}>
+        <TouchableOpacity
+          onPress={() => setActiveTab('friends')}
+          style={{
+            flex: 1,
+            backgroundColor: activeTab === 'friends' ? '#ff9a00' : 'lightgray',
+            borderRadius: 30,
+          }}
+        >
+          <Text style={{ padding: 16, textAlign: 'center', fontWeight: activeTab === 'friends' ? 'bold' : 'normal' }}>Friends</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setActiveTab('chatRooms')}
+          style={{
+            flex: 1,
+            backgroundColor: activeTab === 'chatRooms' ? '#ff9a00' : 'lightgray',
+            borderRadius: 30,
+          }}
+        >
+          <Text style={{ padding: 16, textAlign: 'center', fontWeight: activeTab === 'chatRooms' ? 'bold' : 'normal' }}>Chat Rooms</Text>
+        </TouchableOpacity>
+      </View>
+
+      {activeTab === 'friends' && (
+        <FlatList
+          data={friendsData}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => handleFriendSelection(item)}>
+              <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                backgroundColor: 'lightgray',
+                marginHorizontal: 10,
+                marginTop: 15,
+                borderRadius: 12,
+                paddingHorizontal: 16,
+                paddingVertical: 10,
+              }}>
+                <View style={{
+                  width: 50,
+                  height: 50,
+                  borderRadius: 25,
+                  backgroundColor: 'gray',
+                  marginRight: 10,
+                }}>
+                </View>
+
+                <View style={{ flex: 1 }}>
+                  <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>{item.name}</Text>
+                  <Text>Ultimul mesaj trimis</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      )}
+
+      {activeTab === 'chatRooms' && (
+        <FlatList
+          data={chatRooms}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => handleChatRoomSelection(item)} style={{ alignItems: 'center' }}>
+              <View style={{ flexDirection: 'column', padding: 16, alignItems: 'center', width: 300, backgroundColor: 'lightgray', marginHorizontal: 10, marginTop: 15, borderRadius: 12, height: 65 }}>
+                <Text>{item.name}</Text>
+                <Text style={{ fontWeight: 'bold' }}>Members: </Text>
+              </View>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      )}
     </View>
   );
 };
-
 export default HomeScreen;
